82606b0bbf6a53b962025819fcf46e123184c6cd
SANJEEVI KUMAR <sanjeevik89@gmail.com>
2022-07-10 17:40:07 +0000

    first commit

---
+++ app/unlock_pdf.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, File, UploadFile, Form
+from pydantic import BaseModel, Field
+
+
+app = FastAPI()
+
+
+
+def _find_next_id():
+    return max(country.country_id for country in countries) + 1
+
+class Country(BaseModel):
+    country_id: int = Field(default_factory=_find_next_id, alias="id")
+    name: str
+    capital: str
+    area: int
+
+countries = [
+    Country(id=1, name="Thailand", capital="Bangkok", area=513120),
+    Country(id=2, name="Australia", capital="Canberra", area=7617930),
+    Country(id=3, name="Egypt", capital="Cairo", area=1010408),
+]
+
+@app.get("/countries")
+async def get_countries():
+    return countries
